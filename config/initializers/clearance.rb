cd60ab7026adc509be7434218e03f903364699b1
katechurilova <katechurilova@gmail.com>
2014-06-17 17:42:49 +0300

    using gems3

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,10 +1,10 @@
 Clearance.configure do |config|
   config.allow_sign_up = true
-  config.cookie_domain = '.example.com'
+  config.cookie_domain = '.gmail.com'
   config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
   config.cookie_path = '/'
   config.httponly = false
-  config.mailer_sender = 'reply@example.com'
+  config.mailer_sender = 'imdb@gmail.com"'
   config.password_strategy = Clearance::PasswordStrategies::BCrypt
   config.redirect_url = '/'
   config.secure_cookie = false
